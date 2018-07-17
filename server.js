b03012113ec04f5cdf61ed9e5839b0ff6b01e3ce
Katy <ihatetoast@gmail.com>
2018-07-17 09:01:46 -0500

    changed how data stored. readied for a db for later... ish

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
+
+
 //GALT: GREYHOUND ADOPTION LEAGUE OF TEXAS
 /*
 deal with empty name strings and "adoption pending"
 */
+request(`http://galtx.org/hounds/available.shtml`, function (err, response, html) {
+  if (!err && response.statusCode == 200) {
+    //whole damn page
+    console.log(html);
+    //the goods:
+    var $ = cheerio.load(html);
+    var houndData = [];
+    $("ul.media-grid li").each(function (i, el) {
+      var name = $(el).text().trim();
+      var nameUrl = $(el).text().trim().toLowerCase();
+      var a = $(el).children().attr("href");
+      var link = `http://galtx.org/hounds/${a}`;
+      var img = `http://galtx.org/images/hounds/${nameUrl}_thm.jpg`;
+      var houndMeta = { name, link, img };
+      houndData.push(houndMeta)
 
-request(`http://galtx.org/hounds/available.shtml`, function (err, response, htmlData) {
+    });
+    console.log(houndData);
 
-  var $ = cheerio.load(htmlData);
+  }
 
-  var houndData = [];
-
-  $("ul.media-grid li").each(function (i, el) {
-    var name = $(el).text().trim();
-    var nameUrl = $(el).text().trim().toLowerCase();
-    var a = $(el).children().attr("href");
-    var link = `http://galtx.org/hounds/${a}`;
-    var img = `http://galtx.org/images/hounds/${nameUrl}_thm.jpg`
-    houndData.push({ name, link, img })
-
-  });
-  console.log(houndData);
 });
