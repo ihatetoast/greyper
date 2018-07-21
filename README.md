e986eb977d6e4f2f74d626b12c0a0ae625228dc8
Katy <ihatetoast@gmail.com>
2018-07-21 12:48:16 -0500

    galt central texas scraped. NEEDS fixer fcn for its special issues.

--- README.md
+++ README.md
@@ -17,6 +17,10 @@ Building a scraper for images of greyhounds currently available.
 Adoption groups to scrape (mebbeh):
 
 - [x] GALT: Greyhound Adoption League of Texas
-- [ ] GALT-CenTex: Greyhound Adoption League of Texas (ATX)
+- [x] GALT-CenTex: Greyhound Adoption League of Texas (ATX)
 - [ ] GALT-New Mexico: Greyhound Adoption League of Texas (in NM)
 - [ ] VAGA - Virginia Greyhound Adoption group
+
+---
+
+
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,7 +10,6 @@ module.exports = function (app) {
   //GALT: GREYHOUND ADOPTION LEAGUE OF TEXAS
   app.get("/api/galt", function (req, res) {
     // res.send("greyhound adoption league of texas");
-
     request(`http://galtx.org/hounds/available.shtml`, function (err, response, html) {
       if (!err && response.statusCode == 200) {
         //console.log(html); //returns whole damn page
@@ -25,6 +24,27 @@ module.exports = function (app) {
           var a = $(el).children().attr("href");
           var link = `http://galtx.org/hounds/${a}`;
           var img = `http://galtx.org/images/hounds/${nameUrl}_thm.jpg`;
+          var houndMeta = { name, link, img };
+          houndData.push(houndMeta)
+        });
+        res.send(houndData);
+      }
+    });
+  });
+
+  //GALT-CENTEX: GREYHOUND ADOPTION LEAGUE OF TEXAS
+  app.get("/api/centex", function (req, res) {
+    // res.send("greyhound adoption league of texas");
+    request(`https://galtx-centex.org/greyhounds/`, function (err, response, html) {
+      if (!err && response.statusCode == 200) {
+        var $ = cheerio.load(html);
+        var houndData = ["boop"];
+        $("a.thumbnail").each(function (i, el) {
+          var nameData = $(el).text().trim();
+          var name = nameFix(nameData);
+          var nameUrl = name.toLowerCase();
+          var link = `https://galtx-centex.org/greyhounds/${nameUrl}`;
+          var img = `https://galtx-centex.org/img/thm/${nameUrl}.jpg`;
           var houndMeta = { name, link, img };
           houndData.push(houndMeta)
         });
