2b56adb6921acb1f0229de36a6acfe4aff1be679
Katy <ihatetoast@gmail.com>
2018-08-06 17:20:03 -0500

    updated readme to show states and states' progress

--- app/routing/apiRoutes/vaApiRoutes.js
+++ app/routing/apiRoutes/vaApiRoutes.js
@@ -4,6 +4,8 @@ const cheerio = require('cheerio');
 //TODO: functions rewritten after tests. also renamed.
 //apply new fcns here.
 //vaga gets hounds from orlando. when i get to florida, deal with the duplicate
+//HELPER FCNS NEEDED
+// DEALING WITH EMPTY FIELDS ... AFTER VA IS DONE
 import { getVagaName } from '../../../helpers/helpers';
 
 module.exports = function(app) {
@@ -37,7 +39,11 @@ module.exports = function(app) {
             var img = $('.wp1link')
               .children()
               .html();
-            var houndMeta = { name, link, img };
+            var houndMeta = {
+              name,
+              link,
+              img
+            };
             console.log(houndMeta);
             houndDataVaga.push(houndMeta);
           }
@@ -48,6 +54,7 @@ module.exports = function(app) {
   });
   //OLD DOMINION gets hounds from alabama.
   //alabama groups separates by males and females because everyone hates me.
+  //http://www.ohmygreyhounds.com/adoptable-females.html
   app.get('/api/odga-males', function(req, res) {
     request(`http://www.ohmygreyhounds.com/adoptable-males.html`, function(
       err,
@@ -64,7 +71,39 @@ module.exports = function(app) {
           var imgTextTail = $('.wsite-image a img').attr('src');
           var img = `http://www.ohmygreyhounds.com${imgTextTail}`;
           var link = 'http://www.ohmygreyhounds.com/adoptable-males.html';
-          var houndMeta = { nameData, img, link };
+          var houndMeta = {
+            nameData,
+            img,
+            link
+          };
+          // console.log($('.wsite-image a img').attr('src'));
+          houndDataOdgaM.push(houndMeta);
+        });
+        res.send(houndDataOdgaM);
+      }
+    });
+  });
+  app.get('/api/odga-females', function(req, res) {
+    request(`http://www.ohmygreyhounds.com/adoptable-females.html`, function(
+      err,
+      response,
+      html
+    ) {
+      if (!err && response.statusCode == 200) {
+        var $ = cheerio.load(html);
+        var houndDataOdgaM = [];
+        $('.wsite-section-elements').each(function(i, el) {
+          var nameData = $(el)
+            .find('.wsite-content-title')
+            .text();
+          var imgTextTail = $('.wsite-image a img').attr('src');
+          var img = `http://www.ohmygreyhounds.com${imgTextTail}`;
+          var link = 'http://www.ohmygreyhounds.com/adoptable-females.html';
+          var houndMeta = {
+            nameData,
+            img,
+            link
+          };
           // console.log($('.wsite-image a img').attr('src'));
           houndDataOdgaM.push(houndMeta);
         });
